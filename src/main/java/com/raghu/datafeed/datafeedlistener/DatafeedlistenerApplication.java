094e4702b907a037ed013a288a4007ce19cfdeb5
raghu8 <rvmanthena@gmail.com>
2020-08-23 16:35:34 -0400

    Project Files

---
+++ src/main/java/com/raghu/datafeed/datafeedlistener/DatafeedlistenerApplication.java
@@ -0,0 +1,13 @@
+package com.raghu.datafeed.datafeedlistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatafeedlistenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatafeedlistenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raghu/datafeed/datafeedlistener/DatafeedlistenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghu.datafeed.datafeedlistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatafeedlistenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
